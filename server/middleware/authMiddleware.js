1457428644d54217f7559aeb430b3ef6a15ddd02
mustafaevf <knetya274@gmail.com>
2024-10-18 14:28:29 +0300

    [fix]: middleware

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -6,8 +6,6 @@ module.exports = function (req, res, next) {
     }
 
     try {
-        
-        // console.log(req.url + " " + req.headers.authorization);
         const token = req.headers.authorization.split(' ')[1];
         if(!token) {
             return res.status(403).json({message: "dsada"});
@@ -15,7 +13,6 @@ module.exports = function (req, res, next) {
 
         const decoded = jwt.verify(token, "123123");
         req.user = decoded;
-        // console.log(req.user);
         
         next();
     } catch (error) {
